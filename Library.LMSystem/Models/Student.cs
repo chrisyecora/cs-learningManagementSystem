ce83393a9fcfeadd8b92d50c3c589cda52634455
christopheryecora <cey21b@fsu.edu>
2023-02-17 00:01:39 -0500

    Student class complete for #3

---
+++ Library.LMSystem/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Library.LMSystem.Models
+{
+    public class Student : Person
+    {
+        // Grades
+        public Dictionary<String, float> Grades {
+            get;
+            set;
+        }
+
+        // Classification
+        public String Classification {
+            get;
+            set;
+        }
+
+        public override String Display => $"[{Id}] {Name} - {Classification}";
+
+        public Student()
+        {
+            Grades = new Dictionary<string, float>();
+            Classification = string.Empty;
+        }
+    }
+}
+
