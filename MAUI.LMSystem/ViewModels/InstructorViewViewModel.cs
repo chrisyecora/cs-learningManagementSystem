d86527c4318a6cf1888642816f5d4821ad474160
christopheryecora <cey21b@fsu.edu>
2023-04-12 16:37:45 -0400

    Moved things to Instructor View, Closes #35

---
+++ MAUI.LMSystem/ViewModels/InstructorViewViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.Input;
+using Library.LMSystem.Services;
+using MAUI.LMSystem.Popups;
+using MAUI.LMSystem.Views;
+
+namespace MAUI.LMSystem.ViewModels
+{
+    public partial class InstructorViewViewModel : IQueryAttributable
+    {
+        public InstructorViewViewModel()
+        {
+        }
+
+        private StudentService studentService {
+            get;
+            set;
+        }
+        private CourseService courseService {
+            get;
+            set;
+        }
+
+        public void CreatePerson(ContentPage page) {
+            var popup = new CreatePersonPopup(studentService);
+            if (popup != null) {
+                page.ShowPopup(popup);
+            }
+        }
+
+        public void ViewPersons(ContentPage page) {
+            
+        }
+
+        public void CreateCourse(ContentPage page) {
+            var popup = new CreateCoursePopup(courseService);
+            if (popup != null) {
+                page.ShowPopup(popup);
+            }
+        }
+
+        [RelayCommand]
+        void ViewCourses() {
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("courseService", courseService);
+            Shell.Current.GoToAsync("//CoursesPage", parameters);
+        }
+
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            studentService = query["studentService"] as StudentService;
+            courseService = query["courseService"] as CourseService;
+        }
+    }
+}
+
--- MAUI.LMSystem/Views/InstructorView.xaml.cs
+++ MAUI.LMSystem/Views/InstructorView.xaml.cs
@@ -1,14 +1,34 @@
 ﻿namespace MAUI.LMSystem.Views;
+using MAUI.LMSystem.ViewModels;
 
 public partial class InstructorView : ContentPage
 {
     public InstructorView()
     {
         InitializeComponent();
+        BindingContext = new InstructorViewViewModel();
     }
 
     void BackClicked(System.Object sender, System.EventArgs e)
     {
         Shell.Current.GoToAsync("//MainPage");
+    }
+
+    void CreatePersonButton_Clicked(System.Object sender, System.EventArgs e) {
+        (BindingContext as InstructorViewViewModel).CreatePerson(this);
+    }
+
+    void UpdatePersonButton_Clicked(System.Object sender, System.EventArgs e) {
+    }
+
+    void ViewPersonButton_Clicked(System.Object sender, System.EventArgs e) {
+        (BindingContext as InstructorViewViewModel).ViewPersons(this);
+    }
+
+    void CreateCourseButton_Clicked(System.Object sender, System.EventArgs e) {
+        (BindingContext as InstructorViewViewModel).CreateCourse(this);
+    }
+
+    void ManageCoursesButton_Clicked(System.Object sender, System.EventArgs e) {
     }
 }
